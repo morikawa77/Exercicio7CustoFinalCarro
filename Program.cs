3ed6f7d11004963f3a8c81c96f9cf3a1da89796c
Reginaldo Morikawa <reginaldo@wolfandpartners.com.br>
2017-08-12 05:53:43 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercicio7CustoFinalCarro
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double custoFabrica, lucro, impostos, custoFinal, indLucro, indImposto;
+            indLucro = 0.28;
+            indImposto = 0.45;
+			Console.WriteLine("Digite o custo de fábrica do carro: ");
+			custoFabrica = Convert.ToDouble(Console.ReadLine());
+            lucro = Math.Round(custoFabrica * indLucro, 2);
+            impostos = Math.Round(custoFabrica * indImposto, 2);
+            custoFinal = custoFabrica + impostos + lucro;
+            Console.Write("O custo final do carro é " + custoFinal);
+			Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Exercício 7 - Calcular custo final de carro com imposto e lucro do distribuidor C# Curso Programação de Dispositivos Móveis SENAC
